0307616255f7f4366bca779cb478efb93db4128b
vonssy <kak.reyvaldizakaria@gmail.com>
2024-10-26 20:20:44 +0700

    added code task completion

--- bot.py
+++ bot.py
@@ -50,6 +50,20 @@ class CatsDogs:
         minutes, seconds = divmod(remainder, 60)
         return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
     
+    def load_task_list(self):
+        url = "https://raw.githubusercontent.com/vonssy/Response.JSON/refs/heads/main/catsdogs_tasks.json"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            return data.get('code', [])
+        except requests.exceptions.RequestException as e:
+            self.log(f"{Fore.RED + Style.BRIGHT}Error: Failed to fetch data from URL. {e}{Style.RESET_ALL}")
+            return []
+        except json.JSONDecodeError:
+            self.log(f"{Fore.RED + Style.BRIGHT}Error: Failed to parse JSON data.{Style.RESET_ALL}")
+            return []
+    
     def user_info(self, query: str):
         url = 'https://api.catsdogs.live/user/info'
         self.headers.update({
@@ -97,7 +111,7 @@ class CatsDogs:
         response.raise_for_status()
         return response.json()
     
-    def complete_tasks(self, query: str, task_id: str):
+    def complete_basic_tasks(self, query: str, task_id: int):
         url = 'https://api.catsdogs.live/tasks/claim'
         data = json.dumps({'task_id': task_id})
         self.headers.update({
@@ -114,18 +128,36 @@ class CatsDogs:
                 return None
         else:
             return None
+        
+    def complete_video_tasks(self, query: str, video_id: int, code: str):
+        url = 'https://api.catsdogs.live/tasks/claim'
+        data = json.dumps({'task_id': video_id, 'verification_code':code})
+        self.headers.update({
+            'Content-Type': 'application/json',
+            'X-Telegram-Web-App-Data': query
+        })
+
+        response = self.session.post(url, headers=self.headers, data=data)
+        if response.status_code == 200:
+            result = response.json()
+            if result['status'] == 'success':
+                return result
+            else:
+                return None
+        else:
+            return None
     
     def process_query(self, query: str):
+
         user = self.user_info(query)
         if user:
             balance = self.balance(query)
             if balance:
                 total_balance = sum(balance.values())
                 self.log(
-                    f"{Fore.CYAN+Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                    f"{Fore.MAGENTA+Style.BRIGHT}[ Account{Style.RESET_ALL}"
                     f"{Fore.WHITE+Style.BRIGHT} {user['username']} {Style.RESET_ALL}"
-                    f"{Fore.CYAN+Style.BRIGHT}]{Style.RESET_ALL}"
-                    f"{Fore.MAGENTA+Style.BRIGHT}[ Balance{Style.RESET_ALL}"
+                    f"{Fore.MAGENTA+Style.BRIGHT}] [ Balance{Style.RESET_ALL}"
                     f"{Fore.WHITE+Style.BRIGHT} {total_balance} $FOOD {Style.RESET_ALL}"
                     f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
                 )
@@ -133,20 +165,13 @@ class CatsDogs:
                 self.log(f"{Fore.RED+Style.BRIGHT}[ Error Fetching User Info ]{Style.RESET_ALL}")
         else:
             self.log(f"{Fore.RED+Style.BRIGHT}[ Error Fetching User Info ]{Style.RESET_ALL}")
+        time.sleep(1)
 
-        print(
-            f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
-            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
-            f"{Fore.YELLOW + Style.BRIGHT}[ Claiming Game... ]{Style.RESET_ALL}",
-            end="\r",
-            flush=True
-        )
-        time.sleep(1.5)
         claim = self.claim_game(query)
         if claim:
             self.log(
                 f"{Fore.MAGENTA+Style.BRIGHT}[ Game{Style.RESET_ALL}"
-                f"{Fore.GREEN+Style.BRIGHT} Claimed ]{Style.RESET_ALL}"
+                f"{Fore.GREEN+Style.BRIGHT} Is Claimed {Style.RESET_ALL}"
                 f"{Fore.MAGENTA+Style.BRIGHT}] [ Reward{Style.RESET_ALL}"
                 f"{Fore.WHITE+Style.BRIGHT} {claim['claimed_amount']} $FOOD {Style.RESET_ALL}"
                 f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
@@ -154,64 +179,75 @@ class CatsDogs:
         else:
             self.log(
                 f"{Fore.MAGENTA+Style.BRIGHT}[ Game{Style.RESET_ALL}"
-                f"{Fore.YELLOW+Style.BRIGHT} Already Claim Game {Style.RESET_ALL}"
+                f"{Fore.YELLOW+Style.BRIGHT} Is Already Claimed {Style.RESET_ALL}"
                 f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
             )
+        time.sleep(1)
 
-        print(
-            f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
-            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
-            f"{Fore.YELLOW + Style.BRIGHT}[ Get Available Tasks... ]{Style.RESET_ALL}",
-            end="\r",
-            flush=True
-        )
-        time.sleep(1.5)
         tasks = self.tasks(query)
-        completed_tasks = False
         if tasks:
+            processed_videos = set()
+
             for task in tasks:
                 task_id = task['id']
                 title = task['title']
                 reward = task['amount']
+                task_type = task['type']
 
                 if not task['hidden'] and task['transaction_id'] is None:
-                    print(
-                        f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
-                        f"{Fore.MAGENTA + Style.BRIGHT}[ Task{Style.RESET_ALL}"
-                        f"{Fore.WHITE + Style.BRIGHT} {title} {Style.RESET_ALL}"
-                        f"{Fore.YELLOW + Style.BRIGHT}is Strarting...{Style.RESET_ALL}"
-                        f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}",
-                        end="\r",
-                        flush=True
-                    )
-                    time.sleep(2)
 
-                    complete = self.complete_tasks(query, task_id)
-                    if complete:
-                        self.log(
-                            f"{Fore.MAGENTA+Style.BRIGHT}[ Task{Style.RESET_ALL}"
-                            f"{Fore.WHITE+Style.BRIGHT} {title} {Style.RESET_ALL}"
-                            f"{Fore.GREEN+Style.BRIGHT}is Completed{Style.RESET_ALL}"
-                            f"{Fore.MAGENTA+Style.BRIGHT} ] [ Reward{Style.RESET_ALL}"
-                            f"{Fore.WHITE+Style.BRIGHT} {reward} $FOOD {Style.RESET_ALL}"
-                            f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
-                        )
+                    if task_type == 'video_code':
+                        tasks_video = self.load_task_list()
+
+                        for video in tasks_video:
+                            video_id = video['id']
+                            code = video['code']
+
+                            if video_id in processed_videos:
+                                continue
+
+                            complete_video = self.complete_video_tasks(query, video_id, code)
+                            if complete_video:
+                                self.log(
+                                    f"{Fore.MAGENTA + Style.BRIGHT}[ Task{Style.RESET_ALL}"
+                                    f"{Fore.WHITE + Style.BRIGHT} {video['title']} {Style.RESET_ALL}"
+                                    f"{Fore.GREEN + Style.BRIGHT}Is Completed{Style.RESET_ALL}"
+                                    f"{Fore.MAGENTA + Style.BRIGHT} ] [ Reward{Style.RESET_ALL}"
+                                    f"{Fore.WHITE + Style.BRIGHT} {reward} $FOOD {Style.RESET_ALL}"
+                                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                                )
+                                processed_videos.add(video_id)
+                            else:
+                                self.log(
+                                    f"{Fore.MAGENTA + Style.BRIGHT}[ Task{Style.RESET_ALL}"
+                                    f"{Fore.WHITE + Style.BRIGHT} {video['title']} {Style.RESET_ALL}"
+                                    f"{Fore.RED + Style.BRIGHT}Isn't Completed{Style.RESET_ALL}"
+                                    f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
+                                )
+                            time.sleep(1)
+
                     else:
-                        self.log(
-                            f"{Fore.MAGENTA+Style.BRIGHT}[ Task{Style.RESET_ALL}"
-                            f"{Fore.WHITE+Style.BRIGHT} {title} {Style.RESET_ALL}"
-                            f"{Fore.RED+Style.BRIGHT}Failed{Style.RESET_ALL}"
-                            f"{Fore.MAGENTA+Style.BRIGHT} ]{Style.RESET_ALL}              "
-                        )
-                else:
-                    completed_tasks = True
-
-            if completed_tasks:
-                self.log(f"{Fore.GREEN+Style.BRIGHT}[ All Available Task is Already Completed ]{Style.RESET_ALL}")           
+                        complete_basic = self.complete_basic_tasks(query, task_id)
+                        if complete_basic:
+                            self.log(
+                                f"{Fore.MAGENTA + Style.BRIGHT}[ Task{Style.RESET_ALL}"
+                                f"{Fore.WHITE + Style.BRIGHT} {title} {Style.RESET_ALL}"
+                                f"{Fore.GREEN + Style.BRIGHT}Is Completed{Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT} ] [ Reward{Style.RESET_ALL}"
+                                f"{Fore.WHITE + Style.BRIGHT} {reward} $FOOD {Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                            )
+                        else:
+                            self.log(
+                                f"{Fore.MAGENTA + Style.BRIGHT}[ Task{Style.RESET_ALL}"
+                                f"{Fore.WHITE + Style.BRIGHT} {title} {Style.RESET_ALL}"
+                                f"{Fore.RED + Style.BRIGHT} Isn't Completed{Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
+                            )
+                        time.sleep(1)
         else:
-            self.log(f"{Fore.RED+Style.BRIGHT}[ Error Fetching Tasks Info ]{Style.RESET_ALL}")
-    
+            self.log(f"{Fore.RED + Style.BRIGHT}[ Error Fetching Tasks Info ]{Style.RESET_ALL}")
+        
     def main(self):
         try:
             with open('query.txt', 'r') as file:
@@ -229,9 +265,8 @@ class CatsDogs:
                 for query in queries:
                     query = query.strip()
                     if query:
-                        print(f"{Fore.YELLOW+Style.BRIGHT}[ Getting User Query... ]{Style.RESET_ALL}", end="\r", flush=True)
-                        time.sleep(1.5)
                         self.process_query(query)
+                        time.sleep(3)
                         self.log(f"{Fore.CYAN + Style.BRIGHT}-----------------------------------------------------------------------{Style.RESET_ALL}")
 
                 seconds = 1800
